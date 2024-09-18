2d9f20a4dcc77daec3dc5c4ff24c0d1a22d03a49
Raymond Langas <r_langas@fanshaweonline.ca>
2024-09-18 10:36:52 -0400

    Created a class diagram of current implementation

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
  * Tasks and Possible Changes:
  *
  * General
- * - create separate class from constants so adjusting values will be easier
  * - might want to implement abstract interfaces for Model and Controller
  *   following Observer pattern
  *
@@ -21,23 +20,6 @@
  *
  * Controller
  * - Break out timer task to a separate class as having it inside is messy
- *
- */
-
-/**
- *                    ********** READ ME **********
- *
- * The project right now is implemented following the Model-View-Controller
- * pattern, as to abstract out the data logic, GUI, and functional logic.
- * The Model will deal with all the data logic (setting, calculating, etc.).
- * The View will simply be just the visual/GUI components.
- * The Controller is the functional logic that mediates between the two.
- *      *Note that the Model and View should never directly interact*
- *
- * Concerning the view, it is currently implemented using the Javax Swing GUI
- * library but its would be better. JavaFX uses FXML which is much more
- * intuitive for design, allows for CSS styling, and the further abstraction
- * of the GUI from its Button/Event handlers
  *
  */
 
